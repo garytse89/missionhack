b061fba345cabd55b0d8a9eaf6c128c4f9b0988d
Gary Tse <gtse@copperleaf.com>
2018-03-17 17:07:07 -0700

    Doesnt work navigation

--- App.js
+++ App.js
@@ -1,85 +1,80 @@
 import React, { Component } from 'react';
-import { Container, Header, Content } from 'native-base';
+import { Left, Container, Header, Content, Title, Body, Item, Button, Icon, Input, Text } from 'native-base';
+
+import { List, ListItem, Thumbnail } from 'native-base';
+
 import { Col, Row, Grid } from 'react-native-easy-grid';
-export default class LayoutExample extends Component {
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+import { ItemView } from './item-view';
+
+
+export default class ShoppingListComponent extends Component {
   render() {
     return (
       <Container>
-        <Header />
+        <Header searchBar rounded>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Item>
+            <Icon name="ios-search" />
+            <Input placeholder="Search" />
+            <Icon name="ios-people" />
+          </Item>
+          <Button transparent>
+            <Text>Search</Text>
+          </Button>
+        </Header>
         <Content>
-          <Grid>
-            <Col style={{ backgroundColor: '#635DB7', height: 200 }}></Col>
-            <Col style={{ backgroundColor: '#00CE9F', height: 200 }}></Col>
-          </Grid>
+          <List>
+            <ListItem>
+              <Thumbnail square size={80} source={{ uri: 'https://www.weshareapps.com/wp-content/uploads/2017/06/unnamed-4-52.png' }} />
+              <Body>
+                <Text>Anna</Text>
+                <Text note>Just Nomad Things.</Text>
+              </Body>
+            </ListItem>
+            <ListItem>
+              <Thumbnail square size={80} source={{ uri: 'Image URL' }} />
+              <Body>
+                <Text>Shri</Text>
+                <Text note></Text>
+              </Body>
+            </ListItem>
+            <ListItem>
+              <Thumbnail square size={80} source={{ uri: 'Image URL' }} />
+              <Body>
+                <Text>Gary</Text>
+                <Text note>Its time to build a difference . .</Text>
+              </Body>
+            </ListItem>
+            <ListItem>
+              <Thumbnail square size={80} source={{ uri: 'Image URL' }} />
+              <Body>
+                <Text>Will</Text>
+                <Text note>Its time to build a difference . .</Text>
+              </Body>
+            </ListItem>
+            <ListItem>
+              <Thumbnail square size={80} source={{ uri: 'Image URL' }} />
+              <Body>
+                <Text>Gary</Text>
+                <Text note>Its time to build a difference . .</Text>
+              </Body>
+            </ListItem>
+          </List>
         </Content>
       </Container>
     );
   }
 }
 
-
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  * @flow
-//  */
-
-// import React, { Component } from 'react';
-// import {
-//   Platform,
-//   StyleSheet,
-//   View
-// } from 'react-native';
-// import { Container, Button, Text } from 'native-base';
-
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' +
-//     'Cmd+D or shake for dev menu',
-//   android: 'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu',
-// });
-
-// type Props = {};
-// export default class App extends Component<Props> {
-//   render() {
-//     return (
-//       <Container>
-//         <Button>
-//           <Text>
-//             Button
-//           </Text>
-//         </Button>
-//       </Container>
-//       // <View style={styles.container}>
-//       //   <Text style={styles.welcome}>
-//       //     Welcome to React Native!
-//       //   </Text>
-//       //   <Text style={styles.instructions}>
-//       //   WOW
-//       //   </Text>
-//       //   <Text style={styles.instructions}>
-//       //     {instructions}
-//       //   </Text>
-//       // </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
+const App = StackNavigator({
+  Home: { screen: ShoppingListComponent }, // HOME
+  Item: { screen: ItemView },
+});
